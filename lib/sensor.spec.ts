0dbddfec728deb054c7781762c456f8a51533af7
Felipe Lalanne <1822826+pipex@users.noreply.github.com>
2023-04-08 19:15:18 -0400

    Add test for Sensor.wait()

--- lib/sensor.spec.ts
+++ lib/sensor.spec.ts
@@ -1,7 +1,10 @@
 import { expect } from '~/tests';
 import { Sensor, Subscriber } from './sensor';
+import { promisify } from 'util';
 
 import { stub } from 'sinon';
+
+const delay = promisify(setTimeout);
 
 describe('Sensor', () => {
 	it('only starts execution once subscribers have been added', () => {
@@ -33,6 +36,22 @@ describe('Sensor', () => {
 			result(next(0));
 		});
 
+		expect(result).to.have.been.calledWith(123);
+	});
+
+	it('allows waiting for the sensor to provide a value', async () => {
+		const sensor = Sensor.of(async (subscriber: Subscriber<number>) => {
+			await delay(1000);
+			subscriber.next((x) => x + 123);
+		});
+
+		const result = stub();
+		sensor.subscribe((next: (s: number) => number) => {
+			result(next(0));
+		});
+
+		await expect(sensor.wait(100)).to.be.rejected;
+		await expect(sensor.wait(1000)).to.be.fulfilled;
 		expect(result).to.have.been.calledWith(123);
 	});
 });
