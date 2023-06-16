627cb8f11c54acbb2ec0356549a6ba3beb5fd3f2
Felipe Lalanne <1822826+pipex@users.noreply.github.com>
2023-06-16 10:43:54 -0400

    Create utility tasks for operating on state
    This creates some utility tasks to perform some common operations in
    the state of the system.
    
    - A `NoOp` task can be used for debugging under some conditions
    - A `Pure` task is a task that only performs transforms on the local
      state without affecting the system (e.g. a cleanup)
    - A `Constructor` task is used for initializing an element of the state
    
    Change-type: minor

--- lib/task.ts
+++ lib/task.ts
@@ -6,7 +6,7 @@ import { Op, Operation } from './operation';
 import assert from './assert';
 
 export const NotImplemented = () => Promise.reject('Not implemented');
-export const NoOp = <T>(s: T) => Promise.resolve(s);
+export const NoAction = <T>(s: T) => Promise.resolve(s);
 export const NoEffect = <T>(s: T) => s;
 
 type TaskOp = Op | '*';
@@ -397,6 +397,70 @@ export const Action = {
 
 export const Instruction = {
 	equals: isEqual,
+};
+
+// A pure task does not have an `action` property
+// and requires that `op` is defined
+type PureTaskProps<
+	TState = any,
+	TPath extends Path = '/',
+	TOp extends TaskOp = 'update',
+> = Partial<Omit<ActionTask<TState, TPath, TOp>, 'effect' | 'action' | 'op'>> &
+	Pick<ActionTask<TState, TPath, TOp>, 'effect' | 'op'>;
+
+/**
+ * A pure task is a task that has no side effects,
+ * that is, it does not interact with the underlying system being
+ * controlled, it only transforms the internal state of the agent/planner
+ * in some way.
+ */
+export const Pure = {
+	of: <TState = any, TPath extends Path = '/', TOp extends Op = 'update'>({
+		effect,
+		...props
+	}: PureTaskProps<TState, TPath, TOp>) =>
+		Task.of({
+			effect,
+			...props,
+			action: (s, c) => Promise.resolve(effect(s, c)),
+		}),
+};
+
+/**
+ * A noop task is a task that does not change the state, it can
+ * be added to the plan under some condition for debugging purposes
+ */
+export const NoOp = {
+	of: <TState = any, TPath extends Path = '/'>({
+		description = 'NoOp',
+		...props
+	}: Partial<
+		Pick<
+			ActionTaskProps<TState, TPath, '*'>,
+			'path' | 'condition' | 'description'
+		>
+	>) =>
+		Task.of({
+			description,
+			...props,
+			effect: NoEffect,
+			action: NoAction,
+		}),
+};
+
+/**
+ * A constructor task is a pure task that is used to initialize
+ * the value of a property in the internal state of the system.
+ * It is useful in the case of nested properties, where creating the
+ * parent property has no effects on the system but is necessary in order
+ * for a task for the creation of the child property to be picked up.
+ */
+export const Constructor = {
+	of: <TState = any, TPath extends Path = '/'>(
+		props: Omit<PureTaskProps<TState, TPath, 'create'>, 'op'>,
+	) =>
+		// Only execute the task if the property does not exist
+		Pure.of({ op: 'create', condition: (s, c) => c.get(s) == null, ...props }),
 };
 
 export default Task;
