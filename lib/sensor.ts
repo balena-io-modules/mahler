ca7f83fd86e3771f5dba9d1a93629b28bdfc4094
Felipe Lalanne <1822826+pipex@users.noreply.github.com>
2023-09-27 12:29:15 -0300

    Improve Observable API.
    Observables now are composable via `map` and `flatMap`, they can also
    now accept async iterators as input, which should simplify the way
    sensors and action observables are defined.
    
    Change-type: minor

--- lib/sensor.ts
+++ lib/sensor.ts
@@ -1,12 +1,42 @@
-import { Observable, Observer } from './observable';
+import { Observable, Observer, Subject } from './observable';
 
 export { Subscription } from './observable';
 
-export type Sensor<T> = Observable<(s: T) => T>;
-export type Subscriber<T> = Observer<(s: T) => T>;
+type SensorOutput<T> = (s: T) => T;
+export type Sensor<T> = Observable<SensorOutput<T>>;
+export type Subscriber<T> = Observer<SensorOutput<T>>;
 
 export const Sensor = {
 	of: <T>(
 		sensor: (subscriber: Subscriber<T>) => void | Promise<void>,
-	): Sensor<T> => Observable.of(sensor),
+	): Sensor<T> => {
+		const subject = new Subject<SensorOutput<T>>();
+
+		let running = false;
+		return Observable.from({
+			subscribe(next) {
+				const subscription = subject.subscribe(next);
+
+				// Now that we have subscribers we start the observable
+				if (!running) {
+					Promise.resolve(sensor(subject))
+						.then(() => {
+							// Notify the proxy of the observable completion
+							subject.complete();
+						})
+						.catch((e) => {
+							// Notify subscriber of uncaught errors on the observable
+							subject.error(e);
+						})
+						.finally(() => {
+							// The observable will restart when a new subscriber is added
+							running = false;
+						});
+					running = true;
+				}
+
+				return subscription;
+			},
+		});
+	},
 };
