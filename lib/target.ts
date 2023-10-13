2572c91c2210ebc34f6047ca28c8f59bb9597902
Felipe Lalanne <1822826+pipex@users.noreply.github.com>
2023-10-13 18:48:30 -0300

    Rename Target.DELETED to UNDEFINED
    The UNDEFINED name is more intuitive and makes sense in the context of
    javascript
    
    Change-type: minor

--- lib/target.ts
+++ lib/target.ts
@@ -1,5 +1,5 @@
-export const DELETED: unique symbol = Symbol();
-export type DELETED = typeof DELETED;
+export const UNDEFINED: unique symbol = Symbol('m_undefined');
+export type UNDEFINED = typeof UNDEFINED;
 
 type IsOptional<S extends object, K extends keyof S> = Omit<S, K> extends S
 	? true
@@ -11,7 +11,7 @@ export type Target<S> = S extends any[] | ((...args: any) => any)
 	? {
 			[P in keyof S]?: IsOptional<S, P> extends true
 				? // Only optional properties can be deleted
-				  Target<S[P]> | DELETED
+				  Target<S[P]> | UNDEFINED
 				: Target<S[P]>;
 	  }
 	: S;
